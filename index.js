2443e51ef26dd16e3fcf1d0aa34a8ae88adc7f15
Nishanth <nishanth.kkps@gmail.com>
2024-01-20 23:53:38 +0530

    seperate css files and toggle side bar

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-import express from "express";
 
-const app = express();
-
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-
-app.listen(3000, () => {
-  console.log("Server working ✅");
-});
