fa937f39b706f9df20bda4a2c17dfa39389498f5
Nishanth <nishanth.kkps@gmail.com>
2024-02-27 22:46:04 +0530

    mail

--- schedule.js
+++ schedule.js
@@ -1,32 +1,32 @@
 var nodemailer = require("nodemailer");
 
-// var sub = document.getElementById("sub");
+var sub = document.getElementById("sub");
 
-// sub.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   alert("Your appointment is scheduled");
-//   var fullname = document.getElementById("fullname");
-//   var email = document.getElementById("email");
-//   var phonenumber = document.getElementById("phonenumber");
-//   var textarea = document.getElementById("textarea");
+sub.addEventListener("click", (e) => {
+  e.preventDefault();
+  alert("Your appointment is scheduled");
+  var fullname = document.getElementById("fullname");
+  var email = document.getElementById("email");
+  var phonenumber = document.getElementById("phonenumber");
+  var textarea = document.getElementById("textarea");
 
-//   var date = document.getElementById("date");
-//   var time = document.getElementById("time");
-//   var reason = document.getElementById("reason");
-//   var doctor = document.getElementById("doctor");
+  var date = document.getElementById("date");
+  var time = document.getElementById("time");
+  var reason = document.getElementById("reason");
+  var doctor = document.getElementById("doctor");
 
-//   var detaile = {
-//     fullname: fullname.value,
-//     email: email.value,
-//     phonenumber: phonenumber.value,
-//     textarea: textarea.value,
-//     date: date.value,
-//     time: time.value,
-//     reason: reason.value,
-//     doctor: doctor.value,
-//   };
-//   console.log(detaile);
-// });
+  var detaile = {
+    fullname: fullname.value,
+    email: email.value,
+    phonenumber: phonenumber.value,
+    textarea: textarea.value,
+    date: date.value,
+    time: time.value,
+    reason: reason.value,
+    doctor: doctor.value,
+  };
+  console.log(detaile);
+});
 
 var transporter = nodemailer.createTransport({
   service: "gmail",
@@ -40,7 +40,7 @@ var mailOptions = {
   from: "7733nishanthks@gmail.com",
   to: "7733nishanthks@gmail.com",
   subject: "Sending Email using Node.js",
-  text: "That was easy!sssghhjjghh",
+  text: "That was easy!",
 };
 
 transporter.sendMail(mailOptions, function (error, info) {
